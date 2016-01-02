cddf686688d6fc536327a46c27afa7c00380db68
araynaud <arthur.raynaud@icix.com>
2016-01-01 17:27:22 -0800

    git page cleanup

--- tests/git.php
+++ tests/git.php
@@ -1,5 +1,5 @@
 <!doctype html>
-<html ng-app="app">
+<html>
 <head>
 <title>git update</title>
 <meta charset="utf-8">
@@ -9,16 +9,11 @@
 <meta name="mobile-web-app-capable" content="yes" />
 <link rel="stylesheet" href="../../bootstrap/css/bootstrap.css">
 </head>
-<body class="container-fluid" ng-controller="LayoutController as lc">
-
-<?php
-require_once("../include/includes.php");
-//setContentType("text", "plain");
-
-$gitPath = getExePath($exe="GIT", $key="_GIT");
-?>
+<body class="container">
 <br/>
 <pre><?php
+require_once("../include/includes.php");
+$gitPath = getExePath($exe="GIT", $key="_GIT");
 if(!$gitPath || !file_exists($gitPath)) 
 	echo "git disabled.";
 else
@@ -29,7 +24,6 @@ else
 	echo execCommand(makeCommand("[0] pull --rebase", $gitPath));
 }
 ?></pre>
-
 <a href="..">return to application</a>
 </body>
 </html>
