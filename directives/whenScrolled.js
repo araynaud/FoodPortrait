099b9e537b360bceb77d1cbb235263d12da43d89
araynaud <arthur.raynaud@icix.com>
2016-03-29 00:11:46 -0700

    Adding onScrollH directive

--- directives/whenScrolled.js
+++ directives/whenScrolled.js
@@ -1,4 +1,5 @@
-angular.module('app').directive('whenScrolled', function() 
+angular.module('app')
+.directive('whenScrolled', function() 
 {
     console.log("whenScrolled directive");
 
@@ -32,6 +33,51 @@ angular.module('app').directive('whenScrolled', function()
                 console.log("whenScrolled " + el[0].id + ": " + el[0].scrollTop + " / " + maxTop);
                 if (el[0].scrollTop >= maxTop)
                     scope.$apply(attr.whenScrolled);
+            });
+    };
+})
+.directive('onScrollH', function() 
+{
+    console.log("onScrollH directive");
+
+    var tagName = function(el)
+    {
+        return el[0].nodeName.toLowerCase();
+    } 
+
+    return function(scope, el, attr) 
+    {
+        console.log("onScrollH " + attr.onScrollH);
+        if(!attr.onScrollH) return;
+
+        var tag = tagName(el);
+        if(tag == "body" || tag == "html")
+            angular.element(window).bind('scroll', function(evt) 
+            {
+                var max = document.documentElement.scrollWidth - window.innerWidth;
+                console.log("onScrollH: " + window.scrollX + " / " + max);
+                if (window.scrollX <= 0)
+                {
+                    console.log("onScrollH reached left");
+                    scope.$apply(attr.onScrollH);
+                }
+                if (window.scrollX >= max)
+                {
+                    console.log("onScrollH reached right");
+                    scope.$apply(attr.onScrollH);
+                }
+            });
+        else
+            el.bind('scroll', function(evt) 
+            {
+                var rect = el[0].getBoundingClientRect();
+                var maxTop = el[0].scrollWidth - rect.width;
+                console.log("onScrollH " + el[0].id + ": " + el[0].scrollLeft + " / " + max);
+                if (el[0].scrollLeft >= max)
+                {
+                    console.log("onScrollH reached right");
+                    scope.$apply(attr.onScrollH);
+                }
             });
     };
 });
